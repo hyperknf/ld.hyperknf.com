d45d5dc7e32c3dc9a7b39d0c03cd8c8fd617257f
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-05-02 20:58:06 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("Script successfuly loaded")
+
+function Invite() {
+    window.location.href = "https://discord.gg/JXMjf2qbej"
+}
+
+function Rules() {
+    window.location.href = "https://ld.hyperknf.com/rules"
+}
+
+function Appeal() {
+    window.location.href = "https://ld.hyperknf.com/appeal"
+}
