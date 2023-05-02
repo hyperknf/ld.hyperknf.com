b02bb9bb5ab322e87145d9728eda9208eae5ef2f
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-05-02 20:56:33 +0800

    Delete main.js

--- rules/assets/main.js
+++
@@ -1,6 +0,0 @@
-console.log("%cThis is the start of the console", "color: red;")
-console.log("%cDO NOT PASTE ANYTHING HERE UNLESS YOU UNDERSTAND WHAT YOU'RE DOING\nYOU'RE GIVING HACKERS ACCESS TO YOUR PERSONAL INFORMATIONS IF YOU DO SO", "color: red; font-size: 40;")
-
-function Invite() {
-    window.location.href = "https://discord.gg/JXMjf2qbej"
-}
