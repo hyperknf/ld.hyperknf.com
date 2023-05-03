4977927eabd279d8eb233cf643ea93eaf60166f1
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-05-03 18:35:52 +0800

    Create main.js

---
+++ appeal/main.js
@@ -0,0 +1,53 @@
+let time = 0
+
+function Invalid() {
+    document.querySelector(".main").innerHTML = "Invalid or empty arguments"
+}
+
+function Submit() {
+    const el = (ele) => document.querySelector(`.${ele}`)
+    const inputs = [
+        el("tag").value,
+        el("id").value,
+        el("reason").value,
+        el("length").value,
+        el("again").value,
+        el("understand").value,
+        el("else").value
+    ]
+    for (let i in input) if (String(input[i]).replace(/ /g, "") == "") Invalid()
+    
+    const request = new XMLHttpRequest()
+    request.open("POST", "https://discord.com/api/webhooks/1103257319388762182/cySd5bdvw9tncp-oaX07I75zxER6_XvSoz6YdTZzaabqOxnQHbxCNDQfeP-Xw_2kVcz1")
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(JSON.stringify({
+        id: "1103259279974223902",
+        content: "New appeal received",
+        author: {
+            bot: true,
+            id: "1103257319388762182",
+            username: "Ban Appeals",
+            avatar: "8588ff5c8f759cbf74fe0e5ac7cfacf2",
+            discriminator: "0000"
+        },
+        embeds: [
+            {
+                type: "rich",
+                description: `**__User tag__**\n${inputs[0]}g\n**__User ID__**\n${inputs[1]}\n**__Ban reason__**\n${inputs[2]}\n**__Ban length__**\n${inputs[3]}\n**__Will do again?__**\n${inputs[4]}\n**__Understand punish if trolling__**\n${inputs[5]}\n**__Anything else?__**\n${inputs[6]}\n**__Time taken__**\n${time / 1000 > 10000000 ? ">10000" : time / 1000}s`,
+                color: 16711680,
+                footer: {
+                    text: "From https://ld.hyperknf.com/appeal"
+                }
+            }
+        ]
+    }))
+    localStorage.submitted = "true"
+}
+
+const loop = setInterval(() => {
+    time += 1000 / 30
+    if (localStorage.submitted == "true") {
+        const main = document.querySelector(".main")
+        main.innerHTML = "Received your appeal, please be noted that you can only appeal once<br>You will not receive a reply, please attempt to join the server to know your status"
+    }
+}, 1000 / 30)
