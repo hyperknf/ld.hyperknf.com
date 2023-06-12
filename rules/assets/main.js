733d6b4b88ae9980af598218547b59404d63939c
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-06-12 17:46:42 +0800

    Update main.js

--- rules/assets/main.js
+++ rules/assets/main.js
@@ -1,6 +1,6 @@
 const request = new XMLHttpRequest()
 request.open("GET", "https://requests.hyperknf.com/ld/rules")
 request.send()
-setInterval(() => (document.querySelector(".main").innerHTML = JSON.parse(request.responseText).html), 100)
+setInterval(() => (document.querySelector(".main").innerHTML = JSON.parse((()=>request.responseText))().html), 100)
 
 console.log("Script successfully loaded")
