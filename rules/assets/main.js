57a52fdc46eacfb27dbef8c5f780409b8d7604f2
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-06-12 17:28:37 +0800

    Update main.js

--- rules/assets/main.js
+++ rules/assets/main.js
@@ -1,7 +1,7 @@
 const request = new XMLHttpRequest()
 request.open("GET", "https://requests.hyperknf.com/ld/rules")
 request.send()
-while (!JSON.stringify(request.responseText).startsWith("{")) {
+while (JSON.stringify(request.responseText).startsWith("{")) {
     alert(request.responseText)
 }
 document.querySelector(".main").innerHTML = JSON.parse(request.responseText).html
