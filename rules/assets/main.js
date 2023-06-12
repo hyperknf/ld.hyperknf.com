88abc9ea863ba7ad8b496658b02285566a1db7d4
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2023-06-12 20:15:07 +0800

    Update main.js

--- rules/assets/main.js
+++ rules/assets/main.js
@@ -2,7 +2,7 @@ const request = new XMLHttpRequest().open("GET", "https://requests.hyperknf.com/
     JSON.stringify({})
 )
 while (!JSON.stringify(request.responseText).startsWith("{")) {
-    console.log("Ping")
+    console.log("Ping, data: " + JSON.stringify(request.responseText))
 }
 document.querySelector(".main").innerHTML = JSON.parse(request.responseText).html
 
